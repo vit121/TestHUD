bbc6279633fc2a9cbc505762bdb831657f18ef41
VETAL-RYZEN\Vetal <shmikov.vitaliy@gmail.com>
2024-01-16 16:56:52 +0300

    All images were imported.

---
+++ TestHUD/Helpers/ImageHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace TestHUD.Helpers
+{
+    public class ImageHelper
+    {
+        public static ImageHelper Instance = new ImageHelper();
+
+        public BitmapImage GetImage(string imageName)
+        {
+            return new BitmapImage(new Uri("../Images/" + imageName + ".png", UriKind.Relative));
+        }
+    }
+}
